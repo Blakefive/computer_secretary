981afab939cfcffefe8538f5e0589942cd05b9c5
Chanwoo Lee <fivefive050500@gmail.com>
2020-10-06 23:50:01 +0900

    2020_10_06_computer_secretary

--- weather.py
+++ weather.py
@@ -11,9 +11,8 @@ def weather(time):
     data2 = data1.findAll('dd',{'class' : 'lv2'})
     test = data1.find('span',{'class' : 'merge'}).text
     e1 = data1.findAll('span',{'class':'todaytemp'})
-    t = data1.findAll('span',{'class':'lv1'})
     r1 = data1.findAll('p',{'class':'cast_txt'})
-    p1 = data1.findAll('span',{'class':'lv2'})
+    pp = data1.findAll('span',{'class':'indicator'})
     e = ''
     r = ''
     check = 0
@@ -33,6 +32,6 @@ def weather(time):
     if time == '오늘':
         return '현재 위치는 ' + find_address + '입니다...' +r1[0].text + '최저 온도' + e +'도, 최고 온도' + r+ '도, 현재 온도' + find_currenttemp + '도 입니다' + ' 그리고 미세먼지는 '+str(data2[0].text)[-2:]+' 초미세먼지는 '+str(data2[0].text)[-2:]+' 입니다'
     if time == '내일':
-        return '현재 위치는 ' + find_address + '입니다... 오전에는 ' + r1[1].text + '이고 오후는 ' + r1[2].text + '입니다.' + '오전 온도는 ' + e1[1].text + '도. 오후 온도는 ' + e1[2].text + '도 입니다 그리고 오전 미세먼지는' +t[0].text +'오후 미세먼지는' + t[1].text + '입니다'
+        return '현재 위치는 ' + find_address + '입니다... 오전에는 ' + r1[1].text + '이고 오후는 ' + r1[2].text + '입니다.' + '오전 온도는 ' + e1[1].text + '도. 오후 온도는 ' + e1[2].text + '도 입니다 그리고 오전 미세먼지는' +pp[1].text +'오후 미세먼지는' + pp[2].text + '입니다'
     if time == '모레':
-        return '현재 위치는 ' + find_address + '입니다... 오전에는 ' + r1[3].text + '이고 오후는 ' + r1[4].text + '입니다.' + '오전 온도는 ' +e1[3].text + '도. 오후 온도는 ' + e1[4].text + '도 입니다 그리고 오전 미세먼지는'+p1[1].text +'오후 미세먼지는' + p1[2].text + '입니다'
+        return '현재 위치는 ' + find_address + '입니다... 오전에는 ' + r1[3].text + '이고 오후는 ' + r1[4].text + '입니다.' + '오전 온도는 ' +e1[3].text + '도. 오후 온도는 ' + e1[4].text + '도 입니다 그리고 오전 미세먼지는'+pp[3].text +'오후 미세먼지는' + pp[4].text + '입니다'
